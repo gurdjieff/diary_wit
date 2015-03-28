b458f32ef7a2ec9ccecdce2f2c82e74830ae2f90
DaiyuZhang <gurdjieff100@gmail.com>
2015-03-28 15:08:31 +0000

    layout register and login

--- src/com/diary/activities/LoginActivity.java
+++ src/com/diary/activities/LoginActivity.java
@@ -1,23 +1,50 @@
 package com.diary.activities;
 
 
+import com.diary.activities.MainActivity.MyListener;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import app.diary.R;
 
 public class LoginActivity extends Activity {
+	private Button registerBtn = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.user_login);
+		setContentView(R.layout.login);
+		registerBtn = (Button) this.findViewById(R.id.button2);
+		registerBtn.setOnClickListener(new MyListener());
+
+
 	}
+	
+	class MyListener implements OnClickListener {
+		@Override
+		public void onClick(View v) {
+			switch (v.getId()) {
+			case R.id.button2:
+				Intent intent = new Intent(LoginActivity.this,
+						RegisterActivity.class);
+				startActivity(intent);
+				break;
+			default:
+				break;
+			}
+		}
+	}
+	
+	
 	public void login(View v){
 		EditText edtphone = (EditText)findViewById(R.id.editText2);
 		EditText edtpwd = (EditText)findViewById(R.id.EditText01);
@@ -30,4 +57,5 @@ public class LoginActivity extends Activity {
 					"\n3333:"+edtpwd.getText().toString(), Toast.LENGTH_LONG).show();
 		}
 	}
+	
 }
---
+++ src/com/diary/activities/RegisterActivity.java
@@ -0,0 +1,33 @@
+package com.diary.activities;
+
+import com.diary.activities.LoginActivity.MyListener;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import app.diary.R;
+
+public class RegisterActivity extends Activity {
+	private ImageView back = null;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.register);
+		back = (ImageView) this.findViewById(R.id.back_look_diary);
+		back.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+
+	}
+}
